d477f5b45bd1af80211f2117ae275e6b78aef0de
ammu <ammu@SATISH>
2018-05-09 23:20:32 +0530

    hi projecct ready

---
+++ src/com/git/Employee.java
@@ -0,0 +1,27 @@
+package com.git;
+
+public class Employee {
+	private int eid;
+	private String ename;
+	public Employee() {
+		super();
+	}
+	public int getEid() {
+		return eid;
+	}
+	public void setEid(int eid) {
+		this.eid = eid;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	@Override
+	public String toString() {
+		return "Employee [eid=" + eid + ", ename=" + ename + "]";
+	}
+	
+
+}
